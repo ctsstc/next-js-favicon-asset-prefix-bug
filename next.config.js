591dd37c4265fc43fb6e23f7d9dadd994fc87ec0
Cody Swartz <ctsstc@gmail.com>
2023-08-25 13:19:52 -0700

    conditional asset prefix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const skipSsrAssetProxy = process.env.SKIP_SSR_ASSET_PROXY === "true";
+
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: skipSsrAssetProxy ? undefined : "/assets/",
 };
